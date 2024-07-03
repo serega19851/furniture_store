947b088d7cdf0d85522dd44048dc8de9a929e53a
sa <sergeyyyy1985.an@gmail.com>
2024-07-03 16:30:24 +0300

    Registered simple tag for categories

--- App/main/views.py
+++ App/main/views.py
@@ -6,11 +6,9 @@ from goods.models import Categories
 
 def index(request) -> HttpResponse:
 
-    categories: BaseManager[Categories] = Categories.objects.all()
     context: dict[str, str] = {
         "title": "Home - Главная",
         "content": "Магазин мебели HOME",
-        "categories": categories,
     }
     return render(request, "main/index.html", context)
 
