de39ec1b72ddda26b32ae4c07b20a5c167cb9592
sa <sergeyyyy1985.an@gmail.com>
2024-07-06 11:11:05 +0300

    Slug converter for product

--- App/goods/views.py
+++ App/goods/views.py
@@ -11,5 +11,7 @@ def catalog(request):
     return render(request, "goods/catalog.html", context)
 
 
-def product(request):
-    return render(request, "goods/product.html")
+def product(request, product_slug):
+    product: Products = Products.objects.get(slug=product_slug)
+    context: dict[str, Products] = {"product": product}
+    return render(request, "goods/product.html", context=context)
