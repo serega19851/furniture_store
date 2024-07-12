cf65babd3d912d742cd75d4994e240017915cfae
sa <sergeyyyy1985.an@gmail.com>
2024-07-12 16:22:55 +0300

    The product filter on the site page and urlencode

--- App/goods/views.py
+++ App/goods/views.py
@@ -5,16 +5,23 @@ from goods.models import Products
 
 
 def catalog(request, category_slug) -> HttpResponse:
-    page: Any = request.GET.get('page', 1)
-    print(request)
-    if category_slug== 'all':
+    page: Any = request.GET.get("page", 1)
+    on_sale: Any = request.GET.get("on_sale", None)
+    order_by: Any = request.GET.get("order_by", None)
+    if category_slug == "all":
         goods: BaseManager[Products] = Products.objects.all()
-    else:   
-        goods: BaseManager[Products] = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+    else:
+        goods: BaseManager[Products] = get_list_or_404(
+            Products.objects.filter(category__slug=category_slug)
+        )
 
+    if on_sale:
+        goods: BaseManager[Products] | Any = goods.filter(discount__gt=0)
+
+    if order_by and order_by != "default":
+        goods: BaseManager[Products] | Any = goods.order_by(order_by)
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
-
+    current_page = paginator.page(int(page))
     context: dict[str, Any] = {
         "title": "Home - Каталог",
         "goods": current_page,
