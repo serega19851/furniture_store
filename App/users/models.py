94d571ed55c40fef6a260dfeb9ee196013ecf11d
sa <sergeyyyy1985.an@gmail.com>
2024-07-23 09:25:05 +0300

    Login form and controller are ready

--- App/users/models.py
+++ App/users/models.py
@@ -1,3 +1,18 @@
+from typing import Any
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    image: Any = models.ImageField(
+        upload_to="users_images", blank=True, null=True, verbose_name="Аватар"
+    )
+
+    class Meta:
+        db_table: str = "user"
+        verbose_name: str = "Пользователя"
+        verbose_name_plural: str = "Пользователи"
+
+    def __str__(self) -> str:
+        return self.username
--- App/users/views.py
+++ App/users/views.py
@@ -1,9 +1,32 @@
+from typing import Any
+from django.contrib.auth.models import AbstractBaseUser, auth
+from django.http import HttpResponseRedirect
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
+
+from users.forms import UserLoginForm
 
 
 def login(request) -> HttpResponse:
-    context: dict[str, str] = {"title": "Home - Авторизация"}
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username: Any = request.POST["username"]
+            password: Any = request.POST["password"]
+            user: AbstractBaseUser | None = auth.authenticate(
+                username=username, password=password
+            )
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserLoginForm()
+
+    context: dict[str, str] = {
+        "title": "Home - Авторизация",
+        "form": form,
+    }
     return render(request, "users/login.html", context)
 
 
