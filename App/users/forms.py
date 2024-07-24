310fdb532e38db27c8ad6c37eb5a26b9c071e02e
sa <sergeyyyy1985.an@gmail.com>
2024-07-24 08:28:58 +0300

    Registration form and controller are ready

--- App/users/forms.py
+++ App/users/forms.py
@@ -1,25 +1,34 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm 
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
-from users.models import User 
+from users.models import User
+
 
 class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ["username", "password"]
 
     username = forms.CharField()
     password = forms.CharField()
 
-    # username = forms.CharField(
-    #     label = 'Имя',
-    #     widget=forms.TextInput(attrs={"autofocus": True,
-    #                                   'class': 'form-control',
-    #                                   'placeholder': 'Введите ваше имя пользователя'})
-    # )
-    # password = forms.CharField(
-    #     label = 'Пароль',
-    #     widget=forms.PasswordInput(attrs={"autocomplete": "current-password",
-    #                                       'class': 'form-control',
-    #                                       'placeholder': 'Введите ваш пароль'})
-    # )
+
+class UserRegistrationForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
--- App/users/views.py
+++ App/users/views.py
@@ -1,15 +1,16 @@
 from typing import Any
+
 from django.contrib.auth.models import AbstractBaseUser, auth
 from django.http import HttpResponseRedirect
 from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request) -> HttpResponse:
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
             username: Any = request.POST["username"]
@@ -31,13 +32,30 @@ def login(request) -> HttpResponse:
 
 
 def registration(request) -> HttpResponse:
-    context: dict[str, str] = {"title": "Home - Регистрация"}
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user: Any = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserRegistrationForm()
+
+    context: dict[str, str] = {
+        "title": "Home - Регистрация",
+        "form": form,
+    }
     return render(request, "users/registration.html", context)
 
 
 def profile(request) -> HttpResponse:
-    context: dict[str, str] = {"title": "Home - Кабинет"}
+    context: dict[str, str] = {
+        "title": "Home - Кабинет",
+    }
     return render(request, "users/profile.html", context)
 
 
-def logout(request): ...
+def logout(request):
+    auth.logout(request)
+    return redirect(reverse("main:index"))
