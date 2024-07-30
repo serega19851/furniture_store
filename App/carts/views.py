a346cab2092bedc15f32c47b5f6e8a5f8e70e094
sa <sergeyyyy1985.an@gmail.com>
2024-07-30 12:16:16 +0300

    Cart remove controller addeded commit befor ajax

--- App/carts/views.py
+++ App/carts/views.py
@@ -1,27 +1,29 @@
-from django.http import HttpResponsePermanentRedirect, HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 
 from carts.models import Cart
 from goods.models import Products
 
 
-def cart_add(request, product_slug)-> HttpResponseRedirect | HttpResponsePermanentRedirect:
-    product: Products = Products.objects.get(slug=product_slug)
+def cart_add(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
 
     if request.user.is_authenticated:
-        carts: Cart = Cart.objects.filter(user=request.user, product=product)
+        carts = Cart.objects.filter(user=request.user, product=product)
 
         if carts.exists():
-            cart: Cart | None = carts.first()
+            cart = carts.first()
             if cart:
                 cart.quantity += 1
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
-    return redirect(request.META['HTTP_REFERER'])
+    return redirect(request.META["HTTP_REFERER"])
 
 
 def cart_change(request, product_slug): ...
 
 
-def cart_remove(request, product_slug): ...
+def cart_remove(request, cart_id):
+    cart = Cart.objects.get(id=cart_id)
+    cart.delete()
+    return redirect(request.META["HTTP_REFERER"])
