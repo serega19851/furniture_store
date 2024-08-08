a54518b24b8166e60548ce20d4d68e50f4839889
sa <sergeyyyy1985.an@gmail.com>
2024-08-08 11:19:01 +0300

    Custom validators

--- App/orders/forms.py
+++ App/orders/forms.py
@@ -1,3 +1,5 @@
+import re
+
 from django import forms
 
 
@@ -10,13 +12,24 @@ class CreateOrderForm(forms.Form):
         choices=[
             ("0", False),
             ("1", True),
-            ],
-        )
+        ],
+    )
     delivery_address = forms.CharField(required=False)
     payment_on_get = forms.ChoiceField(
         choices=[
-            ("0", 'False'),
-            ("1", 'True'),
-            ],
-        )
+            ("0", "False"),
+            ("1", "True"),
+        ],
+    )
 
+    def clean_phone_number(self):
+        data = self.cleaned_data["phone_number"]
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+
+        pattern = re.compile(r"^\d{10}$")
+        if not pattern.match(data):
+            raise forms.ValidationError("Неверный формат номера")
+
+        return data
